30202ddac253d95505972ae428497712eba1a4ef
llixing <249568024@qq.com>
2025-03-20 20:07:06 +0800

    init project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,109 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { View } from "react-native";
+import { SymbolView } from "expo-symbols";
+import React from "react";
+import Animated, {
+  useAnimatedProps,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+  interpolateColor,
+} from "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+export function withAnimated<Props extends object>(
+  WrappedComponent: React.FC<Props>
+) {
+  const displayName =
+    WrappedComponent.displayName || WrappedComponent.name || "Component";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+  class WithAnimated extends React.Component<Props> {
+    static displayName = `WithAnimated(${displayName})`;
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    constructor(props: Props) {
+      super(props);
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
   }
 
+  return Animated.createAnimatedComponent<Props>(WithAnimated);
+}
+
+const AnimatedSymbolView = withAnimated(SymbolView);
+
+export default function Index() {
+  // 颜色动画进度
+  const colorProgress = useSharedValue(0);
+
+  const colors = [
+    "#FF0000",
+    "#FF7F00",
+    "#FFFF00",
+    "#00FF00",
+    "#0000FF",
+    "#4B0082",
+    "#9400D3",
+    "#FF0000",
+  ];
+
+  React.useEffect(() => {
+    colorProgress.value = withRepeat(
+      withTiming(1, { duration: 5000 }),
+      -1, // -1 表示无限循环
+      true // true 表示来回动画
+    );
+  }, []);
+
+  const animatedProps = useAnimatedProps(() => {
+    const mainColor = interpolateColor(
+      colorProgress.value,
+      [0, 1 / 6, 2 / 6, 3 / 6, 4 / 6, 5 / 6, 1],
+      colors.slice(0, -1)
+    );
+
+    const bgColor = interpolateColor(
+      colorProgress.value,
+      [0, 1 / 6, 2 / 6, 3 / 6, 4 / 6, 5 / 6, 1],
+      colors.slice(0, -1)
+    );
+
+    return {
+      colors: [mainColor, bgColor],
+    };
+  });
+
+  const otherAnimatedProps = useAnimatedProps(() => {
+    return {
+      tintColor: interpolateColor(
+        colorProgress.value,
+        [0, 1 / 6, 2 / 6, 3 / 6, 4 / 6, 5 / 6, 1],
+        colors.slice(0, -1)
+      ),
+    };
+  });
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AnimatedSymbolView
+        name="chevron.backward.circle.fill"
+        type="palette"
+        animatedProps={animatedProps}
+        // colors={["#000", "#fff"]}
+        size={100}
+      />
+      <AnimatedSymbolView
+        name="chevron.backward.circle.fill"
+        type="palette"
+        animatedProps={otherAnimatedProps}
+        size={100}
+      />
+    </View>
   );
 }
