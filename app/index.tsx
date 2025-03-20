d3e0dec0f21a8f36f0809aefa52d345f203128a3
llixing <249568024@qq.com>
2025-03-20 20:07:53 +0800

    delete layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,88 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { SymbolView } from "expo-symbols";
+import React from "react";
+import Animated, {
+  useAnimatedProps,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+  interpolateColor,
+} from "react-native-reanimated";
+
+export function withAnimated<Props extends object>(
+  WrappedComponent: React.FC<Props>
+) {
+  const displayName =
+    WrappedComponent.displayName || WrappedComponent.name || "Component";
+
+  class WithAnimated extends React.Component<Props> {
+    static displayName = `WithAnimated(${displayName})`;
+
+    constructor(props: Props) {
+      super(props);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  return Animated.createAnimatedComponent<Props>(WithAnimated);
+}
+
+const AnimatedSymbolView = withAnimated(SymbolView);
 
 export default function Index() {
+  // 颜色动画进度
+  const colorProgress = useSharedValue(0);
+
+  const colors = [
+    "#FF0000",
+    "#FF7F00",
+    "#FFFF00",
+    "#00FF00",
+    "#0000FF",
+    "#4B0082",
+    "#9400D3",
+    "#FF0000",
+  ];
+
+  React.useEffect(() => {
+    colorProgress.value = withRepeat(
+      withTiming(1, { duration: 5000 }),
+      -1, // -1 表示无限循环
+      true // true 表示来回动画
+    );
+  }, []);
+
+  const animatedProps = useAnimatedProps(() => {
+    const mainColor = interpolateColor(
+      colorProgress.value,
+      [0, 1 / 6, 2 / 6, 3 / 6, 4 / 6, 5 / 6, 1],
+      colors.slice(0, -1)
+    );
+
+    const bgColor = interpolateColor(
+      colorProgress.value,
+      [0, 1 / 6, 2 / 6, 3 / 6, 4 / 6, 5 / 6, 1],
+      colors.slice(0, -1)
+    );
+
+    return {
+      colors: [mainColor, bgColor],
+    };
+  });
+
+  const otherAnimatedProps = useAnimatedProps(() => {
+    return {
+      tintColor: interpolateColor(
+        colorProgress.value,
+        [0, 1 / 6, 2 / 6, 3 / 6, 4 / 6, 5 / 6, 1],
+        colors.slice(0, -1)
+      ),
+    };
+  });
+
   return (
     <View
       style={{
@@ -9,7 +91,19 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <AnimatedSymbolView
+        name="chevron.backward.circle.fill"
+        type="palette"
+        animatedProps={animatedProps}
+        // colors={["#000", "#fff"]}
+        size={100}
+      />
+      <AnimatedSymbolView
+        name="chevron.backward.circle.fill"
+        type="palette"
+        animatedProps={otherAnimatedProps}
+        size={100}
+      />
     </View>
   );
 }
